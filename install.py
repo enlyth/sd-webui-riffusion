c34f05b7dcf9b5cec3cf95c6525cf28f72e25453
Boris Popik <popik.boris@gmail.com>
2022-12-17 14:49:27 +0000

    Remove mismatched quotes

--- install.py
+++ install.py
@@ -5,15 +5,13 @@ from launch import run
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
-print("")
 print("Initializing Riffusion")
-print("")
 riffusion_skip_install = os.environ.get("RIFFUSION_SKIP_INSTALL", False)
 
 if not riffusion_skip_install:
     name = "Riffusion"
     run(
-        f'"{sys.executable}" -m pip install torchaudio==0.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"',
+        f'"{sys.executable}" -m pip install torchaudio==0.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113',
         f"[{name}] Installing torchaudio...",
         f"[{name}] Couldn't torchaudio.",
     )
