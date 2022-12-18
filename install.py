dea297328e2c4e192bdd3c9813bb62d12e37120c
enlyth <popik.boris@gmail.com>
2022-12-18 03:24:13 +0000

    Fix(?) macOS

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import platform
 from launch import run
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
@@ -10,11 +10,19 @@ riffusion_skip_install = os.environ.get("RIFFUSION_SKIP_INSTALL", False)
 
 if not riffusion_skip_install:
     name = "Riffusion"
-    run(
-        f'"{sys.executable}" -m pip install torchaudio==0.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113',
-        f"[{name}] Installing torchaudio...",
-        f"[{name}] Couldn't torchaudio.",
-    )
+    if platform.system() == "Darwin":
+        # MacOS
+        run(
+            f'"{sys.executable}" -m pip install torchaudio==0.12.1',
+            f"[{name}] Installing torchaudio...",
+            f"[{name}] Couldn't install torchaudio.",
+        )
+    else:
+        run(
+            f'"{sys.executable}" -m pip install torchaudio==0.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113',
+            f"[{name}] Installing torchaudio...",
+            f"[{name}] Couldn't install torchaudio.",
+        )
 
     run(
         f'"{sys.executable}" -m pip install -r "{req_file}"',
