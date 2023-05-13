743034d33bc41a82c6e97940c549878b004636d0
jahu00 <jahu00@users.noreply.github.com>
2023-05-13 19:15:17 +0200

    Fixed the name of torch-directml package

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ riffusion_skip_install = os.environ.get("RIFFUSION_SKIP_INSTALL", False)
 if not riffusion_skip_install:
     name = "Riffusion"
     if not launch.is_installed("torchaudio"):
-        if platform.system() == "Darwin" or launch.is_installed("torch-directml"):
+        if platform.system() == "Darwin" or launch.is_installed("torch_directml"):
             # MacOS and DirectML
             launch.run(
                 f'"{sys.executable}" -m pip install torchaudio==0.13.1',
